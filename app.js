490005cad4687370431786e0e05a305812732f73
Kasun Gunawardana <kasun4it@gmail.com>
2020-08-16 00:30:06 +0530

    Ending Follow Unfollow

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const morgan = require('morgan');
+
+const bodyParser = require('body-parser');
+
+const cookieParser = require('cookie-parser')
+
+const expressValidator = require('express-validator');
+
+const cors = require('cors')
+
+const dotenv = require('dotenv')
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+.then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB Connetion error: ${err.message}`);
+});
+
+const postRoutes = require('./routes/post');
+
+const authRoutes = require('./routes/auth');
+
+const userRoutes = require('./routes/users');
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser())
+app.use(expressValidator());
+app.use(cors());
+app.use("/", postRoutes);
+app.use("/", authRoutes);
+app.use("/", userRoutes);
+
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({error: 'Unauthorized!'});
+    }
+  });
+
+const port = process.env.PORT || 27017;
+
+app.listen(port, () => {
+    console.log('this is a callback funtions from node js');
+});
---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+const User = require('../models/user');
+
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+
+const expressJwt = require('express-jwt');
+
+const { cookie } = require('express-validator/check');
+
+exports.signup = async (req, res) => {
+    const userExists = await User.findOne({email: req.body.email});
+    if(userExists) return res.status(403).json({
+        error: "Email is taken!"
+    });
+
+    const user = await new User(req.body);
+    await user.save();
+    res.status(200).json({message: "Signup Success! Please login."});
+};
+
+exports.signin = async (req, res) => {
+    const { email, password } = req.body;
+    User.findOne(({email}), (err, user) => {
+        if(err || !user) {
+            return res.status(401).json({
+                error: "User with that email does not exist. Please signin."
+            })
+        }
+
+        if(!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Password is wrong"
+            })
+        }
+
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+
+        res.cookie("t", token, {expire:new Date() + 9999})
+
+        const {_id, name, email} = user;
+
+        return res.json({ token, user: {_id, email, name} });
+    }); 
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie("t");
+    return res.json({ message: "Signout success!!"});
+}
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'auth'
+})
